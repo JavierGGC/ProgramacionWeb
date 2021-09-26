6514f76725ffde5b3e8c1a421584b54a8c06db5c
JAVIER GERARDO GUZMÁN CORTEZ <171G0578@rcarbonifera.tecnm.mx>
2021-09-26 14:29:52 -0500

    Agregar archivos de proyecto.

---
+++ Unidad1Actividad1/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Unidad1Actividad1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hola";
+        }
+        public IActionResult MiPerfil()
+        {
+            return View();
+        }
+    }
+}
